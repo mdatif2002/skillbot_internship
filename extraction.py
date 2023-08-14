9bd02a99a0495b122d58f00c567073b0aaef183a
mdatif2002 <142075429+mdatif2002@users.noreply.github.com>
2023-08-14 15:00:59 +0530

    Extraction.py

--- extraction.py
+++ extraction.py
@@ -8,9 +8,9 @@ pin_temp=[]
 with open(fileread,"r") as readfile:
 	file_con=readfile.read()
 for line in file_con.split("\n"):
-		m=re.match("cell ((.*)) {",line)
-		m1=re.match("  area : (.*);",line)
-		m2=re.match("  pin((.*))  {",line)
+		m=file.match("cell ((.*)) {",line)
+		m1=file.match("  area : (.*);",line)
+		m2=file.match("  pin((.*))  {",line)
 		if m:
 			if f==1:
 				pin.append(pin_temp)
